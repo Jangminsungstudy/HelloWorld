f4ddc2b10361d07e3a13b66eb32fb95d751877ba
HelloWorld <yokam4@gmail.com>
2024-03-18 00:38:47 +0900

    테스트

---
+++ WorkSpace_C/StudyFile_2.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    //증감연산
+    int a = 10;
+    printf("%d\n", a);
+    a++;
+    printf("%d\n", a);
+
+    int b = 20;
+    printf("b는 %d\n", ++b); // b+1
+    printf("b는 %d\n", b++); // 다음 줄에서 b + 1 연산
+    printf("b는 %d\n", b); // 따라서 출력값 b = 22
+
+    // 반복문
+    // for, while, do while
+
+    //for (선언, 조건, 증감)
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("Hello World %d\n", i);
+    }
+    //while (조건)
+    int i = 1;
+
+    while (i <= 10)
+    {
+        printf("Hello World %d\n", i++);
+    }
+    //do { code }; while (조건);
+    i = 1;
+    do {
+        printf("Hello World %d\n", i++);
+     } while (i<=10);
+    return 0;
+}
