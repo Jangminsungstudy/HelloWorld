1e40cd2e3967a95b0b65e1cfc4dbf50da7ef62b6
Jangminsungstudy <yokam4@gmail.com>
2024-03-18 15:35:33 +0900

    240318 commit

--- WorkSpace_R/240318_실습파일.R
+++ WorkSpace_R/240318_실습파일.R
@@ -20,4 +20,48 @@ v1 * v2
 v + v1
 
 v4 = c(1, 2)
+v1 + v4
 
+sum(v3)
+mean(v3)
+median(v2)
+max(v2)
+min(v2)
+var(v2) #분산 = 평균으로부터 떨어진 거리 제곱의 평균
+sd(v2)
+sort(v2) #오름차순
+sort(v2, decreasing = TRUE) #내림차순
+range(v2)
+length(v2)
+
+v1 > 5
+v1[v1>5]
+v1[v1>5 & v1<9]
+
+m1 = matrix(v2, nrow=5, ncol=2)
+m2 = matrix(1:25, nrow=5, ncol=5, byrow=T)
+
+#매트릭스 와의 사칙연산을 위해서는 차원이 같아야 함. ex) 5x5 * 5x5 O, 5x2 * 2x5 X
+
+sum(m1)
+mean(m1)
+m3= matrix(rep(2, 25), nrow=5, ncol=5)#2를 25번 반복해서 매트릭스를 생성하라.
+
+m2*m3
+m2-m3
+m2+m3
+m2/m3
+
+m1[,1]
+m2[1,]
+
+is.vector(m2[1,]) #벡터인지 확인하는 함수
+is.numeric(m2)
+is.integer(m2)
+#그 밖에 문자, 정수, 실수 등... is. 함수를 이용하면 가능
+
+rbind(m1, m2)
+cbind(m2, m1)
+cbind(m1, m2)
+rbind(m2, m3)
+#bind 사용법 미리미리 익혀두기!!!!! 데이터프레임 컨트롤에서 필수적으로 사용함.
